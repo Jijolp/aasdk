aadec8b69c514e1ab40d9f0f0de576848bf9422e
Jijolp <johlanlepy@gmail.com>
2026-09-19 22:40:17 -0300

    revert(s3): drop payload dump (decoded: playback on fixed id 9)

--- src/Channel/InputSource/InputSourceChannel.cpp
+++ src/Channel/InputSource/InputSourceChannel.cpp
@@ -16,7 +16,6 @@
 *  along with aasdk. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <cstdio>
 #include <aasdk_proto/InputSourceChannelMessageIdsEnum.pb.h>
 #include <aasdk_proto/ControlMessageIdsEnum.pb.h>
 #include <f1x/aasdk/Channel/InputSource/InputSourceChannel.hpp>
@@ -93,21 +92,9 @@ void InputSourceChannel::messageHandler(messenger::Message::Pointer message, IIn
         this->handleChannelOpenRequest(payload, std::move(eventHandler));
         break;
     default:
-    {
-        // S3 diagnostic: dump unhandled payloads (the phone sends 0x8001 at
-        // 1 Hz on this channel). Revisit/remove once decoded.
-        std::string hex;
-        char byte[4];
-        for(size_t i = 0; i < payload.size && i < 16; ++i)
-        {
-            std::snprintf(byte, sizeof(byte), "%02x", payload.cdata[i]);
-            hex += byte;
-        }
-        AASDK_LOG(error) << "[InputSourceChannel] message not handled: " << messageId.getId()
-                         << ", size: " << payload.size << ", head: " << hex;
+        AASDK_LOG(error) << "[InputSourceChannel] message not handled: " << messageId.getId();
         this->receive(std::move(eventHandler));
         break;
-    }
     }
 }
 
